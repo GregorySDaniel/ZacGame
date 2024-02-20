b85a7c0705529d3e576857cdbda459b033654e8f
Gregory <gregorycedro@gmail.com>
2024-02-20 02:16:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ var score = document.getElementById('score');
 
 const characterImg = document.querySelector('.characterImage img');
 
-var characterX = 0;
-var characterY = 0;
+var characterX = 80;
+var characterY = 30;
 var bloobX = 0;
 var bloobY = 0;
 var enemyX = 0;
 var enemyY = 0;
+var spearX = 0;
+var spearY = 0;
+var utilY = 0;
 var currentScore = 0;
 
 var held_directions = [];
@@ -39,7 +42,7 @@ const placeCharacter = () => {
   const held_direction = held_directions[0];
   if (held_direction) {
     if (held_direction === directions.right) {
-      characterX += speed; 
+      characterX += speed;
       characterImg.src = 'assets/rightZac.png';}
     if (held_direction === directions.left) {
       characterX -= speed;
@@ -60,13 +63,20 @@ function placeBlobs () {
 
 const step = () => {
   placeCharacter();
-  console.log(`X: ${characterX} Y: ${characterY}`)
-  if (Math.abs(bloobX-characterX) <= 12 && Math.abs(bloobX-characterX)>=4 && (bloobY - characterY) >= 18 && (bloobY - characterY) <= 28) { 
-    placeBlobs(); 
-    updateScore();
-  }
+  // console.log(`Personagem Y= ${characterY}`);
+
   window.requestAnimationFrame(()=> {
     step();
+    if (Math.abs(bloobX-characterX) <= 12 && Math.abs(bloobX-characterX)>=4 && (bloobY - characterY) >= 18 && (bloobY - characterY) <= 28) {
+      placeBlobs();
+      updateScore();
+    }
+
+    if (Math.abs(spearX-characterX) < 2 && Math.abs(spearY-characterY)<10)  {
+      console.log("pegou");
+      character.classList.toggle('color');
+      return;
+    }
   })
 }
 
@@ -77,18 +87,46 @@ function updateScore(){
 
 function enemySlide(){
   setTimeout(() => {
+    utilY = enemyY;
     enemys.classList.add('animation');
     enemys.style.transform = `translate3d( ${(20+enemyX)*pixelSize}px, ${enemyY*pixelSize}px, 0)`
     setTimeout(() => {
       enemys.classList.remove('animation');
       enemyAppear();
-    }, 2000);
-  }, Math.floor(Math.random() * 4000)+1000);
+      throwSkill(utilY);
+    }, 1500);
+  }, 1500);
+
+}
+
+function throwSkill(utilY){
+  // console.log(utilY)
+  // console.log(enemyY)
+  spear.classList.add('spear-animation');
+  spearX = 20+enemyX;
+  spearY = utilY;
+  let x = ((enemyY-20)-spearY)/100;
+  spear.style.transform = `translate3d( ${(250+enemyX)*pixelSize}px, ${(enemyY-20)*pixelSize}px, 0)`
+  for(let i=0; i<100; i++){
+    setTimeout(()=>{
+      spearX += 3;
+      spearY += x;
+      // console.log(`Lança X = ${spearX} Lança Y = ${spearY}`);
+    }, 12 * i);
+  }
+  setTimeout(()=> {
+    spear.classList.add('hidden');
+    spear.classList.remove('spear-animation');
+    spear.style.transform = `translate3d( ${(20+enemyX)*pixelSize}px, ${(enemyY-20)*pixelSize}px, 0)`
+    setTimeout(()=>{
+      spear.classList.remove('hidden');
+    }, 1000);
+  }, 1500)
 }
 
 
 function enemyAppear(){
-  enemyX = -20;
+  enemyX = -25;
   enemyY = Math.floor(Math.random() * 70);
   enemys.style.transform = `translate3d( ${enemyX*pixelSize}px, ${enemyY*pixelSize}px, 0)`
   enemySlide();
